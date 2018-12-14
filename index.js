04827452851c14bd4afd926fdeb7141052075fb4
Simen Setrom <simen@4bitgames.com>
2018-12-15 00:06:08 +0100

    generic

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ app.post('/upload/:projectID', jsonParser, async function (req, res) {
     const newData   = req.body;
 
     let projectData = {};
-    if (fs.existsSync(fPath)) {
+    /*if (fs.existsSync(fPath)) {
         projectData = JSON.parse(fs.readFileSync(fPath));
-        client.set(projectID, JSON.stringify({"a":4}));
-        console.log(await getAsync(projectID))
+    }*/
+    try{
+        projectData = await getAsync(projectID)
+        console.log("(upload) SUCCESS get", projectID)
+    }catch(err){
+        console.log("(upload) Can't get", projectID)
     }
-
+    
     let outputData = {};
     for (box in newData){
         if (!projectData[box] || !projectData[box].version || (newData[box].version > projectData[box].version)){
@@ -33,10 +37,14 @@ app.post('/upload/:projectID', jsonParser, async function (req, res) {
         }
     }
 
-    console.log("client",client)
-
-    fs.writeFileSync(fPath, JSON.stringify(projectData, null, 2));
-    console.log("writeFileSync", projectData)
+    //fs.writeFileSync(fPath, JSON.stringify(projectData, null, 2));
+    
+    try{
+        projectData = client.set(projectID, JSON.stringify(projectData, null, 2));
+        console.log("(upload) SUCCESS set", projectID)
+    }catch(err){
+        console.log("(upload) Can't set", projectID)
+    }
 
     console.log("Upload:", fPath)
     res.send(outputData);
@@ -48,10 +56,18 @@ app.post('/download/:projectID', jsonParser, async function (req, res) {
     const manifest  = req.body;
     console.log("manifest",manifest)
 
-    let projectData = {};
+    /*let projectData = {};
     if (fs.existsSync(fPath)) {
         projectData = JSON.parse(fs.readFileSync(fPath));
         console.log("projectData", projectData)
+    }*/
+
+    let projectData = {};
+    try{
+        projectData = await getAsync(projectID)
+        console.log("(download) SUCCESS get", projectID)
+    }catch(err){
+        console.log("(download) Can't get", projectID)
     }
 
     let outputData = {};
